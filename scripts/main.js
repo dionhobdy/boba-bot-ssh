f1a89abea803977d092758476f0a0b845a2ca222
Dion Hobdy <devdionhobdy@gmail.com>
2026-09-05 21:54:16 -0700

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+const input = document.getElementById("command");
+const output = document.getElementById("output");
+
+input.addEventListener("keydown", function(event) {
+    if (event.key !== "Enter") { return; }
+
+    const command = input.value.trim();
+
+    output.innerHTML += ` user@boba-bot:~$ ${command}<br> `;
+    executeCommand(command);
+
+    input.value = "";
+});
+
+function executeCommand(command) {
+    switch (command) {
+        case "help":
+            output.innerHTML += `
+                Available Commands:<br>
+                clear
+                generate
+                help
+                loadMenu
+                ls
+                open
+                saveMenu
+            `;
+            break;
+        default:
+            output.innerHTML += ` Command not found: ${command}<br><br> `;
+    }
+}
